72fe82ed08b836384e6494828ed371fd403cab9e
Tranduy1dol <tmd.id2004@gmail.com>
2025-04-15 17:18:05 +0000

    feat: detach function and add make_prediction

--- lib/data.py
+++ lib/data.py
@@ -1,3 +1,5 @@
+import datetime
+
 import joblib
 import numpy as np
 import pandas as pd
@@ -5,27 +7,71 @@ import ta.momentum as momentum
 import ta.trend as trend
 import ta.volume as volume
 import tensorflow as tf
-
 from binance.client import Client
 from sklearn.preprocessing import MinMaxScaler
 
 
-def transform(x, y):
+def transform(x, y, window_size):
     x_train = []
     y_train = []
-    window_size = 30
 
-    for i in range(window_size, len(x)-2):
-        x_train.append(x[i-window_size:i, :])
-        y_train.append(y[i+2, 0])
+    for i in range(window_size, len(x) - 2):
+        x_train.append(x[i - window_size:i, :])
+        y_train.append(y[i + 2, 0])
 
     x_train, y_train = np.array(x_train), np.array(y_train)
+
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2]))
+    y_train = np.reshape(y_train, (-1, 1))
 
     x_train = tf.convert_to_tensor(x_train, dtype=tf.float32)
     y_train = tf.convert_to_tensor(y_train, dtype=tf.float32)
 
     return x_train, y_train
+
+
+def process(data):
+    dataset = data.loc[:, ['time', 'open', 'high', 'low', 'close', 'volume']]
+
+    dataset['rsi'] = momentum.RSIIndicator(dataset['close']).rsi()
+    dataset['adx'] = trend.adx(high=dataset['high'], low=dataset['low'], close=dataset['close'])
+    dataset['ema'] = trend.ema_indicator(close=dataset['close'])
+    dataset['macd'] = trend.macd_diff(close=dataset['close'])
+    dataset['so'] = momentum.stoch(high=dataset['high'], low=dataset['low'], close=dataset['close'])
+    dataset['vwap'] = volume.volume_weighted_average_price(
+        high=dataset['high'],
+        low=dataset['low'],
+        close=dataset['close'],
+        volume=dataset['volume']
+    )
+    dataset['mfi'] = volume.money_flow_index(
+        high=dataset['high'],
+        low=dataset['low'],
+        close=dataset['close'],
+        volume=dataset['volume']
+    )
+    dataset['cmf'] = volume.chaikin_money_flow(
+        high=dataset['high'],
+        low=dataset['low'],
+        close=dataset['close'],
+        volume=dataset['volume']
+    )
+
+    dataset.dropna(inplace=True)
+
+    return dataset
+
+
+def normalize(dataset, path):
+    y = dataset.filter(['close']).values
+    x = dataset[['close', 'macd', 'rsi', 'mfi', 'so', 'cmf', 'ema']].values
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    x = scaler.fit_transform(x)
+    y = scaler.fit_transform(y)
+    joblib.dump(scaler, path)
+
+    return x, y
 
 
 class Data:
@@ -48,6 +94,7 @@ class Data:
         data_frames = data_frames.iloc[:, :6]
         data_frames.columns = ['time', 'open', 'high', 'low', 'close', 'volume']
         data_frames = data_frames.astype(float)
+        data_frames['time'] = pd.to_datetime(data_frames['time'])
         data_frames.dropna()
 
         self.data = data_frames
@@ -60,54 +107,34 @@ class Data:
         train_data = data.iloc[:train_size]
         return train_data, test_data
 
-    def process(self):
-        dataset, _ = self.split()
-        dataset = dataset.loc[:, ['time', 'open', 'high', 'low', 'close', 'volume']]
 
-        dataset['rsi'] = momentum.RSIIndicator(dataset['close']).rsi()
-        dataset['adx'] = trend.adx(high=dataset['high'], low=dataset['low'], close=dataset['close'])
-        dataset['ema'] = trend.ema_indicator(close=dataset['close'])
-        dataset['macd'] = trend.macd_diff(close=dataset['close'])
-        dataset['so'] = momentum.stoch(high=dataset['high'], low=dataset['low'], close=dataset['close'])
-        dataset['vwap'] = volume.volume_weighted_average_price(
-            high=dataset['high'],
-            low=dataset['low'],
-            close=dataset['close'],
-            volume=dataset['volume']
-        )
-        dataset['mfi'] = volume.money_flow_index(
-            high=dataset['high'],
-            low=dataset['low'],
-            close=dataset['close'],
-            volume=dataset['volume']
-        )
-        dataset['cmf'] = volume.chaikin_money_flow(
-            high=dataset['high'],
-            low=dataset['low'],
-            close=dataset['close'],
-            volume=dataset['volume']
-        )
+def make_prediction(model_path, symbol, scaler_path, window_size):
+    """
+    Predict the close price for tomorrow using the trained model.
 
-        dataset.dropna()
+    :param model_path: Path to the trained model file.
+    :param symbol: Cryptocurrency symbol (e.g., 'BTC').
+    :param scaler_path: Path to the scaler file.
+    :param window_size: Number of days for the sliding window.
+    :return: Predicted close price for tomorrow.
+    """
+    today = datetime.datetime.utcnow().strftime('%b %d %Y')
+    dataset = Data(symbol, 'Jan 01 2018', today)
+    dataset.load()
+    processed_data = process(dataset.data)
 
-        return dataset
+    scaler = joblib.load(scaler_path)
+    x = processed_data[['close', 'macd', 'rsi', 'mfi', 'so', 'cmf', 'ema']].values
+    x = scaler.fit_transform(x)
 
-    def data_info(self):
-        train_data = self.process()
-        train_data.info()
+    x_input = x[-window_size:]
+    x_input = np.expand_dims(x_input, axis=0)
 
-    def describe(self):
-        dataset = self.process()
-        dataset.describe()
+    model = tf.keras.models.load_model(model_path)
+    prediction = model.predict(x_input)
 
-    def normalize(self, path):
-        dataset = self.process()
+    predicted_close = scaler.inverse_transform(
+        np.concatenate((prediction, np.zeros((prediction.shape[0], x.shape[1] - 1))), axis=1)
+    )[0, 0]
 
-        y = dataset.filter(['time']).values
-        x = dataset[['time', 'macd', 'rsi', 'mfi', 'so', 'cmf', 'ema']].values
-
-        scaler = MinMaxScaler(feature_range=(0,1))
-        x = scaler.fit_transform(x)
-        joblib.dump(scaler, path)
-
-        return x, y
+    return predicted_close
